9837f14f526495ff9a7f096d4b97d1734de83584
Albert Stjärne <albert.stjarne@gmail.com>
2019-09-09 11:56:23 +0200

    flips cells when clicking

--- src/Board.js
+++ src/Board.js
@@ -76,11 +76,17 @@ class Board extends Component {
     }
 
     // TODO: flip this cell and the cells around it
+    flipCell(y, x); // flip initial cell
+    flipCell(y, x - 1); // flip left
+    flipCell(y, x + 1); // flip right
+    flipCell(y - 1, x); // flip below
+    flipCell(y + 1, x); // flip above
 
     // win when every cell is turned off
     // TODO: determine is the game has been won
+    let hasWon = false;
 
-    // this.setState({ board, hasWon });
+    this.setState({ board, hasWon });
   }
 
   /** Render game board or winning message. */
@@ -95,7 +101,13 @@ class Board extends Component {
       let row = [];
       for (let x = 0; x < this.props.ncols; x++) {
         let coord = `${y}-${x}`;
-        row.push(<Cell key={coord} isLit={this.state.board[y][x]} />);
+        row.push(
+          <Cell
+            key={coord}
+            isLit={this.state.board[y][x]}
+            flipCellsAroundMe={() => this.flipCellsAround(coord)}
+          />
+        );
       }
       tblBoard.push(<tr key={y}>{row}</tr>);
     }
